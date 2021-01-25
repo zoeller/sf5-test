76b37f37fceecd8973a0ef3bba08bc5126637a97
Mischa Zöller <dev@zoeller.biz>
2021-01-25 11:23:14 +0100

    Error created to check logging.

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -8,7 +8,7 @@ class TestController
 {
     public function index(): Response
     {
-        $number = random_int(0, 100);
+        $number = random_int(0, 100);-
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'

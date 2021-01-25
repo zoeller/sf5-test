d90b7edce93c6d4b9c9c01ad6a55bea1d64e4ca8
Mischa Zöller <dev@zoeller.biz>
2021-01-25 11:25:41 +0100

    Log entry created to check logging.

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class TestController
 {
-    public function index(): Response
+    public function index(LoggerInterface $logger): Response
     {
-        $number = random_int(0, 100);-
+        $logger->error('An error occurred');
+
+        $number = random_int(0, 100);
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
